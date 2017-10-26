13bc4134e3c3f2cb6ae7080232ea481e4aaa8a3c
Peter Tountas <petertountas1@gmail.com>
2017-10-25 19:30:34 -0500

    more updates

--- src/components/ListInput.js
+++ src/components/ListInput.js
@@ -13,23 +13,9 @@ class ListInput extends Component{
     return(
       <div style={{marginTop: 35}} className="container">
         <form>
-          <div style={{marginBottom: 10}}>
-            <label>Day:<span></span>
-              <select style={{marginLeft: 10}}>
-                <option defaultValue="">Select</option>
-                <option value="sunday">Sunday</option>
-                <option value="monday">Monday</option>
-                <option value="tuesday">Tuesday</option>
-                <option value="wednesday">Wednesday</option>
-                <option value="thursday">Thursday</option>
-                <option value="friday">Friday</option>
-                <option value="saturday">Saturday</option>
-              </select>
-            </label>
-          </div>
           <div>
             <label>To Do Item: 
-            <input onChange={(e) => this.setState({items: e.target.value})} style={{marginLeft: 10}} lable="nutsack" className="formGroup" placeholder="..." />
+            <input value={this.state.value} onChange={(e) => this.setState({items: e.target.value})} style={{marginLeft: 10}} lable="nutsack" className="formGroup" placeholder="..." />
             </label>
           </div>
           <div>
